de0ebac39118c0ccf5a5ca22a689a3bde1b52da3
Mariana <marianaarevalo933@gmail.com>
2023-09-28 17:02:54 -0700

    Revert "Update main.py"
    This reverts commit 2f4592011fa6b2921c072ee9e663d0cddba30320.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 print('Ejercicio 3 clase-270923')
 
-<<<<<<< HEAD
 print('Ya no somos equipo TEODIO')
-=======
-print('Mariana Arevalo Hernandez')
->>>>>>> parent of 42ae086 (Merge branch 'master' into arevalo)
